b9decde3b21ea4c95a3be4568cfb5d39222c7448
ArtemBystrovOfficial <92841151+ArtemBystrovOfficial@users.noreply.github.com>
2022-05-04 02:04:50 +0800

    Add files via upload

---
+++ Examples/Example 3/ExampleServer.cpp
@@ -0,0 +1,31 @@
+﻿#include "FServer.h"
+
+struct Pocket
+{
+};
+
+int main()
+{
+
+	Server <Pocket> sv("192.168.0.200", 80);
+
+	sv.start();
+
+
+	while (true)
+	{
+		int n = -1;
+			while (n == -1) {
+				n = sv.get_http_connect();
+				Sleep(100);
+			}
+
+		sv.set_path_download("C:\\Users\\Temas\\Desktop\\");
+
+		sv << HTTP{ "test.html",n };
+
+	}
+	sv.stop();
+
+	
+}
